d7a80241e4e90bf20a80a4f657dbba39e00d7cce
SIddharth3791 <spatel3791@gmail.com>
2025-02-18 18:48:47 -0500

    react components, protected route and pages stub created

---
+++ react_ui/src/pages/notfound.tsx
@@ -0,0 +1,11 @@
+function NotFound(){
+
+    return(
+        <div>
+            <h1>404 NOT FOUND.....</h1>
+            <p> The Page you're looking for doesn't exist!</p>
+        </div>
+    )
+}
+
+export default NotFound;
